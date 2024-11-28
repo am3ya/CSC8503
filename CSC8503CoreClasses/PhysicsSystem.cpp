17a9d33f0135d79c00d7986929a34a74ac62a00e
am3ya <142191552+am3ya@users.noreply.github.com>
2024-11-28 13:31:21 +0000

    Add files via upload

--- CSC8503CoreClasses/PhysicsSystem.cpp
+++ CSC8503CoreClasses/PhysicsSystem.cpp
@@ -192,6 +192,26 @@ a particular pair will only be added once, so objects colliding for
 multiple frames won't flood the set with duplicates.
 */
 void PhysicsSystem::BasicCollisionDetection() {
+	std::vector<GameObject*>::const_iterator first;
+	std::vector<GameObject*>::const_iterator last;
+	gameWorld.GetObjectIterators(first, last);
+
+	for (auto i = first; i != last; ++i) {
+		if ((*i)->GetPhysicsObject() == nullptr) {
+			continue;
+		}
+		for (auto j = i + 1; j != last; ++j) {
+			if ((*j)->GetPhysicsObject() == nullptr) {
+				continue;
+			}
+			CollisionDetection::CollisionInfo info;
+			if (CollisionDetection::ObjectIntersection(*i, *j, info)) {
+				std::cout << "Collision between " << (*i)->GetName() << " and " << (*j)->GetName() << std::endl;
+				info.framesLeft = numCollisionFrames;
+				allCollisions.insert(info);
+			}
+		}
+	}
 }
 
 /*
@@ -256,6 +276,17 @@ void PhysicsSystem::IntegrateAccel(float dt) {
 
 		linearVel += accel * dt; //integrate accel
 		object->SetLinearVelocity(linearVel);
+
+		//Angular stuff
+		Vector3 torque = object->GetTorque();
+		Vector3 angVel = object->GetAngularVelocity();
+
+		object->UpdateInertiaTensor(); //Update tensor vs orientation
+
+		Vector3 angAccel = object->GetInertiaTensor() * torque;
+
+		angVel += angAccel * dt; //Integrate angular accel
+		object->SetAngularVelocity(angVel);
 	}
 }
 
@@ -285,6 +316,20 @@ void PhysicsSystem::IntegrateVelocity(float dt) {
 		//Linear damping
 		linearVel = linearVel * frameLinearDamping;
 		object->SetLinearVelocity(linearVel);
+
+		//Orientation stuff
+		Quaternion orientation = transform.GetOrientation();
+		Vector3 angVel = object->GetAngularVelocity();
+
+		orientation = orientation + (Quaternion(angVel * dt * 0.5f, 0.0f) * orientation);
+		orientation.Normalise();
+
+		transform.SetOrientation(orientation);
+
+		//Damp the angular velocity too
+		float frameAngularDamping = 1.0f - (0.4f * dt);
+		angVel = angVel * frameAngularDamping;
+		object->SetAngularVelocity(angVel);
 	}
 }
 
